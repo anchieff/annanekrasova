3753d3f39cdb5148b695661fe99688b1b0845dea
Anna <averbah-ann@mail.ru>
2020-12-24 18:10:46 +0400

    Add links and smooth scroll

--- #src/js/script.js
+++ #src/js/script.js
@@ -85,3 +85,19 @@ const noise = () => {
 };
 
 noise();
+
+// Smooth scroll
+
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() 
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
